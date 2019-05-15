12065416db751b495dbfb50f7144a1998a072254
kshk9509 <45284189+kshk9509@users.noreply.github.com>
2019-05-16 01:37:22 +0900

    Add files via upload

---
+++ MajorActivity.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MajorActivity  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_major);
+
+    }
+}
